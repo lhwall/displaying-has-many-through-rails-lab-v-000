4e96384e0d84f2d91da493897c6813dc2a0b1a51
Lucy Wall <lhwall@gmail.com>
2018-07-13 14:29:15 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,13 @@
 class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
+
+  def datetime_converter=
+
+  end
+
+  def datetime_converter
+    self.datetime_converter
+  end
+
 end
