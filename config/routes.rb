1b8d1dbd8ccfb47d2254d5a57e1d0c29345bf8c7
Lucy Wall <lhwall@gmail.com>
2018-07-13 14:47:09 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :appointments
+  resources :appointments only: [:show, :create, :new, :update, :edit, :delete]
   resources :patients
   resources :doctors
   resources :doctors
